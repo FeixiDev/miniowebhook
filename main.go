d1a95e0771b0c526daa40c0c6339f103c3914911
shiki <237303802@qq.com>
2023-05-11 18:18:04 +0800

    测试

--- main.go
+++ main.go
@@ -17,27 +17,26 @@ const (
 	WebhookURL       = "https://10.203.1.23:30278/audit/webhook/event"
 )
 
-type TypeMeta struct {
-	// +optional
-	APIVersion string `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty" protobuf:"bytes,1,opt,name=apiVersion"`
-	// +optional
-	Kind string `json:"kind,omitempty" yaml:"kind,omitempty" protobuf:"bytes,2,opt,name=kind"`
-}
-
-type Unknown struct {
-	TypeMeta `json:",inline" protobuf:"bytes,1,opt,name=typeMeta"`
-	// Raw will hold the complete serialized object which couldn't be matched
-	// with a registered type. Most likely, nothing should be done with this
-	// except for passing it through the system.
-	Raw []byte `protobuf:"bytes,2,opt,name=raw"`
-	// ContentEncoding is encoding used to encode 'Raw' data.
-	// Unspecified means no encoding.
-	ContentEncoding string `protobuf:"bytes,3,opt,name=contentEncoding"`
-	// ContentType  is serialization method used to serialize 'Raw'.
-	// Unspecified means ContentTypeJSON.
-	ContentType string `protobuf:"bytes,4,opt,name=contentType"`
-}
-
+//	type TypeMeta struct {
+//		// +optional
+//		APIVersion string `json:"apiVersion,omitempty" yaml:"apiVersion,omitempty" protobuf:"bytes,1,opt,name=apiVersion"`
+//		// +optional
+//		Kind string `json:"kind,omitempty" yaml:"kind,omitempty" protobuf:"bytes,2,opt,name=kind"`
+//	}
+//
+//	type Unknown struct {
+//		TypeMeta `json:",inline" protobuf:"bytes,1,opt,name=typeMeta"`
+//		// Raw will hold the complete serialized object which couldn't be matched
+//		// with a registered type. Most likely, nothing should be done with this
+//		// except for passing it through the system.
+//		Raw []byte `protobuf:"bytes,2,opt,name=raw"`
+//		// ContentEncoding is encoding used to encode 'Raw' data.
+//		// Unspecified means no encoding.
+//		ContentEncoding string `protobuf:"bytes,3,opt,name=contentEncoding"`
+//		// ContentType  is serialization method used to serialize 'Raw'.
+//		// Unspecified means ContentTypeJSON.
+//		ContentType string `protobuf:"bytes,4,opt,name=contentType"`
+//	}
 type Backend struct {
 	url              string
 	senderCh         chan interface{}
@@ -46,61 +45,93 @@ type Backend struct {
 	getSenderTimeout time.Duration
 	stopCh           <-chan struct{}
 }
-type MicroTime struct {
-	time.Time `protobuf:"-"`
-}
 
-type Events struct {
-
-	// AuditLevel at which event was generated
-	Level string
-
-	// Unique audit ID, generated for each request.
-	AuditID string
-	// Stage of the request handling when this event instance was generated.
-	Stage string
-
-	// RequestURI is the request URI as sent by the client to a server.
-	RequestURI string
-	// Verb is the kubernetes verb associated with the request.
-	// For non-resource requests, this is the lower-cased HTTP method.
-	Verb string
-	// Authenticated user information.
-	User string
-
-	ImpersonatedUser string
-
-	SourceIPs []string
-
-	UserAgent string
-
-	ObjectRef *ObjectReference
-
-	ResponseStatus string
-
-	RequestObject Unknown
-
-	ResponseObject Unknown
-
-	RequestReceivedTimestamp MicroTime
-
-	StageTimestamp MicroTime
-
-	Annotations map[string]string
-}
+//type MicroTime struct {
+//	time.Time `protobuf:"-"`
+//}
+//
+//type Events struct {
+//	TypeMeta
+//
+//	// AuditLevel at which event was generated
+//	Level string
+//
+//	// Unique audit ID, generated for each request.
+//	AuditID string
+//	// Stage of the request handling when this event instance was generated.
+//	Stage string
+//
+//	// RequestURI is the request URI as sent by the client to a server.
+//	RequestURI string
+//	// Verb is the kubernetes verb associated with the request.
+//	// For non-resource requests, this is the lower-cased HTTP method.
+//	Verb string
+//	// Authenticated user information.
+//	User string
+//
+//	ImpersonatedUser map[string]string
+//
+//	SourceIPs []string
+//
+//	UserAgent string
+//
+//	ObjectRef *ObjectReference
+//
+//	ResponseStatus string
+//
+//	RequestObject Unknown
+//
+//	ResponseObject Unknown
+//
+//	RequestReceivedTimestamp MicroTime
+//
+//	StageTimestamp MicroTime
+//
+//	Annotations map[string]string
+//}
 
 type Event struct {
-	// Devops project
-	Devops string
-	// The workspace which this audit event happened
-	Workspace string
-	// The cluster which this audit event happened
-	Cluster string
-	// Message send to user.
-	Message string
-	Factory string
+	Devops                   string
+	Workspace                string
+	Cluster                  string
+	Message                  string
+	Level                    string
+	AuditID                  string
+	Stage                    string
+	RequestURI               string
+	Verb                     string
+	User                     User
+	ImpersonatedUser         interface{}
+	SourceIPs                []string
+	UserAgent                string
+	ObjectRef                ObjectRef
+	ResponseStatus           ResponseStatus
+	RequestObject            interface{}
+	ResponseObject           interface{}
+	RequestReceivedTimestamp string
+	StageTimestamp           string
+	Annotations              interface{}
+}
 
-	Events
+type User struct {
+	username string
+	groups   []string
+}
+
+type ObjectRef struct {
+	Resource        string
+	Namespace       string
+	Name            string
+	UID             string
+	APIGroup        string
+	APIVersion      string
+	ResourceVersion string
+	Subresource     string
+}
+
+type ResponseStatus struct {
+	Code     int
+	Metadata map[string]interface{}
 }
 
 type EventList struct {
@@ -230,32 +261,44 @@ func (b *Backend) eventToBytes(event EventList) ([]byte, error) {
 }
 
 func main() {
-	// 创建一个Event实例
 	myEvent := Event{
-		Devops:    "DevOps",
-		Workspace: "Workspace",
-		Cluster:   "Cluster",
-		Events: Events{
-			RequestURI:               "Path",
-			Verb:                     "",
-			Level:                    "None",
-			AuditID:                  "123456",
-			Stage:                    "ResponseComplete",
-			ImpersonatedUser:         "nil",
-			UserAgent:                "User-Agent",
-			RequestReceivedTimestamp: MicroTime{},
-			Annotations:              nil,
-			ObjectRef: &ObjectReference{
-				Resource:        "users",
-				Namespace:       "",
-				Name:            "",
-				UID:             "",
-				APIGroup:        "",
-				APIVersion:      "",
-				ResourceVersion: "",
-				Subresource:     "",
-			},
+		Devops:     "DevOps",
+		Workspace:  "Workspace",
+		Cluster:    "Cluster",
+		Message:    "",
+		Level:      "Metadata",
+		AuditID:    "bmc2077-fc3b-46a5-2434-89066c3ad435",
+		Stage:      "ResponseComplete",
+		RequestURI: "",
+		Verb:       "login",
+		User: User{
+			username: "admin",
+
+			groups: []string{"system:authenticated"},
 		},
+		ImpersonatedUser: nil,
+		SourceIPs:        []string{"10.233.103.145"},
+		UserAgent:        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+		ObjectRef: ObjectRef{
+			Resource:        "bmc",
+			Namespace:       "",
+			Name:            "web",
+			UID:             "",
+			APIGroup:        "",
+			APIVersion:      "",
+			ResourceVersion: "",
+			Subresource:     "",
+		},
+		ResponseStatus: ResponseStatus{
+			Code:     200,
+			Metadata: make(map[string]interface{}),
+		},
+
+		RequestObject:            nil,
+		ResponseObject:           nil,
+		RequestReceivedTimestamp: "2023-05-11T05:28:38.988705Z",
+		StageTimestamp:           "2023-05-11T05:28:38.679121Z",
+		Annotations:              nil,
 	}
 
 	myEventList := EventList{
